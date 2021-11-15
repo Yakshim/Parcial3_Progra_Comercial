e88f454268fb2b78181944d1e71622d8e094dda9
Adolfo Diego <yakshim0@gmail.com>
2021-11-15 02:11:01 -0600

    Creacion de modelos de hotel y huesped

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
+class Hotel(models.Model):
+
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+
+        return self.name
+
+
+class Huesped(models.Model):
+
+    name = models.CharField(max_length=100)
+    direccion = models.TextField()
+    correo = models.TextField()
+    telefono = models.TextField()
+    Hotel = models.ForeignKey(
+
+        Hotel, related_name="Huesped", on_delete=models.CASCADE
+
+    )
+
+
+    def __str__(self):
+
+        return self.name
+
 # Create your models here.
